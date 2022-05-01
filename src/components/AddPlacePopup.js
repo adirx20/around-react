56d78c355a50ea84b263205577bb94eaf986b0f1
adirx20 <87542990+adirx20@users.noreply.github.com>
2022-05-01 23:40:57 +0300

    first time finish2

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import api from '../utils/api';
 import CurrentUserContext from '../contexts/CurrentUserContext';
 import PopupWithForm from './PopupWithForm';
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import '../index.css';
 import logo from '../images/header-logo.svg';
 import api from '../utils/api';
